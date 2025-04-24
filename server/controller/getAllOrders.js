2a3dfc58330c95178dae4d8096c510e4b8f72c6d
FaizAhmad0 <118246870+FaizAhmad0@users.noreply.github.com>
2025-04-24 08:32:26 +0530

    maked it lightweited

--- server/controller/getAllOrders.js
+++ server/controller/getAllOrders.js
@@ -5,7 +5,9 @@ module.exports = async (req, res) => {
     const orders = await User.find(
       {},
       "orders name email enrollment amount manager"
-    );
+    )
+      .sort({ createdAt: -1 }) // sort by most recent
+      .limit(200); // get last 10
 
     res.status(200).json({ orders });
   } catch (error) {
@@ -13,8 +15,6 @@ module.exports = async (req, res) => {
     res.status(500).json({ error: "Could not retrieve user data" });
   }
 };
-
-
 
 // const User = require("../models/User");
 // module.exports = async (req, res) => {
