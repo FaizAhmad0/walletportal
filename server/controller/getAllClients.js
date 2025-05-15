9058eef0a553b10cd582dec83904c29f184f5b90
FaizAhmad0 <118246870+FaizAhmad0@users.noreply.github.com>
2025-05-15 18:01:07 +0530

    in wallet action

--- server/controller/getAllClients.js
+++ server/controller/getAllClients.js
@@ -1,8 +1,24 @@
+// const User = require("../models/User");
+
+// module.exports = async (req, res) => {
+//   try {
+//     const clients = await User.find({ role: "user" });
+
+//     res.status(200).json({ clients });
+//   } catch (error) {
+//     console.error("Error finding clients:", error);
+//     res.status(500).json({ error: "Could not find clients" });
+//   }
+// };
+
 const User = require("../models/User");
 
 module.exports = async (req, res) => {
   try {
-    const clients = await User.find({ role: "user" });
+    const clients = await User.find(
+      { role: "user" },
+      "name email enrollment mobile amount transactions"
+    ); // Only select specific fields
 
     res.status(200).json({ clients });
   } catch (error) {
